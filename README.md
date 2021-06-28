e71eda367e80a47c9025c66419ac3ab08acf5be8
你好2007 <2501482523@qq.com>
2021-06-28 16:19:51 +0800

    搭建基本的项目结构

--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Regexper-Visualization
 正则表达式可视化
+
+开源协议
+---------------------------------------
+[MIT](https://github.com/hai2007/Regexper-Visualization/blob/master/LICENSE)
+
+Copyright (c) 2021 [hai2007](https://hai2007.gitee.io/sweethome/) 走一步，再走一步。
---
+++ src/entry.js
@@ -0,0 +1,20 @@
+import QuickPaper from 'quick-paper';
+
+// 兼容文件
+import '@hai2007/polyfill/Promise.js';
+
+// 引入样式
+import "@hai2007/style/normalize.css";
+
+import App from './App.paper';
+
+// 创建对象
+window.quickPaper = new QuickPaper({
+
+    // 挂载点
+    el: document.getElementById('root'),
+
+    // 启动组件
+    render: createElement => createElement(App)
+
+});
